43454a9e86af6c144e2ffef77e31232a33040f73
sg406651 <sg406651@mimuw.edu.pl>
2023-02-02 13:19:56 +0100

    Added parser, data cleaning function and choosing years function, Also added data to the repository

---
+++ data_functions.py
@@ -0,0 +1,36 @@
+import numpy as np
+import argparse
+
+
+def parse_arguments():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("GDP", help="path to GDP file")
+    parser.add_argument("POP", help="path to population data file")
+    parser.add_argument("CO2", help="path to CO2 emission file")
+    args = parser.parse_args()
+    return args
+
+
+def clean(df):
+    df = df.rename(columns={"Country Name": "Country"})
+    df["Country"] = df["Country"].str.upper()
+    df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
+    return df
+
+
+def years(gdp, pop, co2):
+    gdp_years = np.array(gdp.columns.tolist())
+    gdp_years = gdp_years[4:len(gdp_years)]
+    gdp_years = gdp_years.astype(int)
+    pop_years = np.array(pop.columns.tolist())
+    pop_years = pop_years[4:len(pop_years)]
+    pop_years = pop_years.astype(int)
+    co2_years = co2["Year"].unique()
+    year = np.intersect1d(pop_years, gdp_years)
+    year = np.intersect1d(year, co2_years)
+
+    return year
+
+
+
--- run_analysis.py
+++ run_analysis.py
@@ -1,20 +1,11 @@
 # data analysis script
 import argparse
-import configparser
-import os
+import numpy as np
 import pandas as pd
+import data_functions
 
-dir_name = os.path.dirname(__file__)
-config = configparser.ConfigParser()
-config.read(os.path.join(dir_name), "config.ini")
+args = parse_arguments()
 
-parser = argparse.ArgumentParser()
-parser.add_argument("GDP", help="path to GDP file")
-parser.add_argument("population_file", help="path to population data file")
-parser.add_argument("CO2", help="path to CO2 emission file")
-
-args = parser.parse_args()
-print("GDP parser to: ", args.GDP)
-
-#GDP = pd.read_csv(args.GDP)
-#print(pd.GDP.head(5))
+GDP = pd.read_csv(args.GDP, skiprows=4)
+POP = pd.read_csv(args.POP, skiprows=4)
+CO2 = pd.read_csv(args.CO2)
