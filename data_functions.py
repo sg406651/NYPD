4b4cc33e4cc54386a9dfeb7a074bc183f5c5f802
sg406651 <sg406651@mimuw.edu.pl>
2023-02-04 11:37:00 +0100

    added emission function (not final form)

--- data_functions.py
+++ data_functions.py
@@ -1,6 +1,7 @@
 import numpy as np
 import argparse
 import numpy as np
+import pandas as pd
 
 
 def parse_arguments():
@@ -9,7 +10,12 @@ def parse_arguments():
     parser.add_argument("GDP", help="path to GDP file")
     parser.add_argument("POP", help="path to population data file")
     parser.add_argument("CO2", help="path to CO2 emission file")
+    parser.add_argument("--start", type=int,
+                        default=None, help="start of the analyzing interval")
+    parser.add_argument("--end", type=int,
+                        default=None, help="end of the analyzing interval")
     args = parser.parse_args()
+
     return args
 
 
@@ -32,5 +38,29 @@ def years(gdp, pop, co2):
     year = np.intersect1d(year, co2_years)
     return year
 
-def merge(gdp, pop, co2):
-    
+
+def consistent_format(df, statistics_name: str, interval):
+    df_years = np.array(df.columns)
+    df_years = df_years[4:len(df_years)]
+    df = pd.melt(df, id_vars=["Country"], value_vars=df_years)
+    df.columns = ["Country", "Year", statistics_name]
+    df = df.astype({"Year": int})
+    df = df[df["Year"].isin(interval)]
+    return df
+
+
+def merge(df1, df2):
+    df = pd.merge(df1, df2, how='left', left_on=["Country", "Year"], right_on=["Country", "Year"])
+    return df
+
+
+def save_to_xlsx(df, output_name: str):
+    with pd.ExcelWriter(output_name) as writer:
+        df.to_excel(writer, sheet_name=output_name)
+
+
+def max_emission(merged_df):
+    df_new = merged_df[["Year", "Country", "Per Capita", "Total"]]
+    df_to_xlsx = df_new.groupby("Year")["Per Capita"].nlargest(5)
+    #save_to_xlsx(df_to_xlsx, "Emission.xlsx")
+    return df_to_xlsx
--- run_analysis.py
+++ run_analysis.py
@@ -1,6 +1,7 @@
 # data analysis script
 import pandas as pd
 import data_functions as f
+import numpy as np
 
 args = f.parse_arguments()
 
@@ -8,4 +9,16 @@ GDP = pd.read_csv(args.GDP, skiprows=4)
 POP = pd.read_csv(args.POP, skiprows=4)
 CO2 = pd.read_csv(args.CO2)
 
+GDP = f.clean(GDP)
+POP = f.clean(POP)
+
+interval = f.years(GDP, POP, CO2)
+GDP = f.consistent_format(GDP, "GPD", interval)
+POP = f.consistent_format(POP, "POP", interval)
+CO2 = CO2[CO2["Year"].isin(interval)]
+
+merged_df = f.merge(GDP, POP)
+merged_df = f.merge(CO2, merged_df)
+#f.save_to_xlsx(df, "merged_data.xlsx")
+print(f.max_emission(merged_df))
 
