38a2fa109bd4364ad11dc7590e4dc0b3117cf25c
sg406651 <sg406651@mimuw.edu.pl>
2023-02-06 18:37:46 +0100

    added 10 year change in emission

--- run_analysis.py
+++ run_analysis.py
@@ -1,25 +1,25 @@
 # data analysis script
 import pandas as pd
 import data_functions as f
-import numpy as np
 
+
+# get arguments from parser
 args = f.parse_arguments()
-
+# get dataframes from argparse paths
 GDP = pd.read_csv(args.GDP, skiprows=4)
 POP = pd.read_csv(args.POP, skiprows=4)
 CO2 = pd.read_csv(args.CO2)
-
+# clean GDP P
 GDP = f.clean(GDP)
 POP = f.clean(POP)
 
 interval = f.years(GDP, POP, CO2)
+
 GDP = f.consistent_format(GDP, "GPD", interval)
 POP = f.consistent_format(POP, "POP", interval)
 CO2 = CO2[CO2["Year"].isin(interval)]
 
 merged_df = f.merge(GDP, POP)
-merged_df = f.merge(CO2, merged_df)
-#f.save_to_xlsx(df, "merged_data.xlsx")
-print(f.max_emission(merged_df))
-
-
+merged_df = f.merge(merged_df, CO2)
+f.max_revenue(merged_df)
+f.emission_change(merged_df, interval)
